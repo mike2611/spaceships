ec29a668be636d0bb79a90b00c49c9d2704a9305
SunnySparks <Josediazt6@outlook.com>
2021-11-08 12:53:31 -0600

    Navbar Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../App.css';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -7,10 +9,10 @@ const Navbar = () => (
     <div className="container">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-sm-12">
             <div className="row">
               <div className="col-sm-6 p-3">
-                <span className="blu monB titl">Bookstore CMS</span>
+                <span className="blu monB titl">Space Traveler&apos;s Hub</span>
               </div>
               <div className="col-sm-2 monR linkNav align-padding">
                 <NavLink
@@ -37,9 +39,6 @@ const Navbar = () => (
                 </NavLink>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 justify-content-end">
-            <div className="circle-border pt-3" />
           </div>
         </div>
       </div>

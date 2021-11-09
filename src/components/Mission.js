55bed11dd52ff038a1591360ee48622e3ddc6db3
mike2611 <mpuentes2611@gmail.com>
2021-11-09 15:09:19 -0600

    Add style to btns

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -2,22 +2,29 @@ import PropTypes from 'prop-types';
 
 const Mission = (props) => {
   const {
-    missionId,
     missionName,
     description,
   } = props;
 
   return (
-    <>
-      <p>{missionId}</p>
-      <p>{missionName}</p>
-      <p>{description}</p>
-    </>
+    <tr>
+      <th scope="row"><h2 className="btn mission-name">{missionName}</h2></th>
+      <th>{description}</th>
+      <th className="align-middle">
+        <button className="not-member-btn" type="button">
+          NOT A MEMBER
+        </button>
+      </th>
+      <th className="align-middle">
+        <button className="join-mission-btn" type="button">
+          Join Mission
+        </button>
+      </th>
+    </tr>
   );
 };
 
 Mission.propTypes = {
-  missionId: PropTypes.string.isRequired,
   missionName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -15,16 +15,29 @@ const Misions = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Missions Info</h1>
-      {missions.map((mission) => (
-        <Mission
-          key={`${mission.mission_id}`}
-          missionId={`${mission.mission_id}`}
-          missionName={`${mission.mission_name}`}
-          description={`${mission.description}`}
-        />
-      ))}
+    <div className="d-flex justify-content-center">
+      <div className="mission-container mt-4">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th scope="col"><h2>Mission</h2></th>
+              <th scope="col"><h2>Description</h2></th>
+              <th scope="col"><h2>Status</h2></th>
+              <th scope="col"> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {missions.map((mission) => (
+              <Mission
+                key={`${mission.mission_id}`}
+                missionId={`${mission.mission_id}`}
+                missionName={`${mission.mission_name}`}
+                description={`${mission.description}`}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

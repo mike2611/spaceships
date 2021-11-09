f685d83d1f5fb0ffc45e7912d771d4d0a8dbd1da
mike2611 <mpuentes2611@gmail.com>
2021-11-09 16:48:40 -0600

    Add joinMission action to missionsREducer

--- src/redux/missions/missionsReducer.js
+++ src/redux/missions/missionsReducer.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const misionsURL = 'https://api.spacexdata.com/v3/missions';
 const GET_MISSIONS = 'spaceships/misions/GET_MISSIONS';
+const JOIN_MISSION = 'spaceships/misions/JOIN_MISSON';
 
 const initialState = [];
 
@@ -18,10 +19,24 @@ export const getMissions = () => async (dispatch) => {
   }
 };
 
+export const joinMission = (missionId, dispatch) => {
+  dispatch({
+    type: JOIN_MISSION,
+    payload: missionId,
+  });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MISSIONS:
       return action.payload;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.payload) {
+          return mission;
+        }
+        return { ...state, reserved: true };
+      });
     default:
       return state;
   }

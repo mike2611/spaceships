cbd73038cdb4274ce34b9b491d1435bb03694ac7
SunnySparks <Josediazt6@outlook.com>
2021-11-09 22:02:35 -0600

    Display Rockets and info on UI

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../App.css';
+import './rocket.css';
 import React from 'react';
 // import { useDispatch } from 'react-redux';
 // import { getRockets } from '../redux/rockets/rockets';
@@ -12,15 +13,22 @@ const RocketList = (props) => {
     rocketName, description, flickrImages,
   } = props;
   return (
-    <div className="container">
+    <div className="container p-4">
       <div className="row">
         <div className="col-sm-4">
-          <img src={flickrImages} alt="user" className="userImage" />
+          <img src={flickrImages} alt="user" className="rocketImage" />
         </div>
         <div className="col-sm-8">
           <div className="row">
-            {rocketName}
+            <h3>
+              {rocketName}
+            </h3>
+          </div>
+          <div className="row">
             {description}
+          </div>
+          <div className="row">
+            <button type="button" className="btn btn-primary col-sm-4">RESERVE ROCKET</button>
           </div>
         </div>
       </div>
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,19 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../App.css';
+import './rocket.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import RocketList from './RocketList';
 import { getRockets } from '../redux/rockets/rockets';
 
 const Rocket = () => {
+  const rocketArr = [];
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getRockets());
   }, []);
 
-  const rocketArr = [];
   const rocketlist = useSelector((state) => state.rocketsReducer);
   const rockets = rocketlist;
   console.log(rocketlist);
@@ -37,6 +38,11 @@ const Rocket = () => {
   ));
   const rocketInfo = rocketArr[0];
   console.log(rocketInfo);
+  useEffect(() => {
+    if (rocketArr.length === 0) {
+      dispatch(getRockets());
+    }
+  }, []);
   return (
     <div>
       <div>

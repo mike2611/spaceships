3977f88ee837cf9df0164b51d4b2adb3329535b9
SunnySparks <Josediazt6@outlook.com>
2021-11-10 16:19:27 -0600

    Create rocket booking

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const GET_ROCKETS_SUCCESS = 'GET_ROCKETS_SUCCESS';
+const BOOK_ROCKET = 'BOOK_ROCKET';
 const API_URL = 'https://api.spacexdata.com/v3/rockets';
 
 const initialState = [];
@@ -16,10 +17,22 @@ export const getRockets = () => async (dispatch) => {
     });
 };
 
+export const bookRocket = (payload) => ({
+  type: BOOK_ROCKET,
+  payload,
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS_SUCCESS:
-      return action.payload;
+      return action.payload.data.map((state) => ([state, { reserved: false }]));
+    case BOOK_ROCKET:
+      return state.map((rocket) => {
+        if (rocket[0].rocket_id !== action.payload) {
+          return rocket;
+        }
+        return Object.assign([rocket[0], { reserved: true }]);
+      });
     default:
       return state;
   }
